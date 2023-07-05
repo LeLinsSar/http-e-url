3d8ab360cbee666732d7888fc379b39359d9b3f6
Letícia <110118449+LeLinsSar@users.noreply.github.com>
2023-07-05 17:50:21 -0300

    Add files via upload

---
+++ atividade http e url/core_module_http_atv_1/1_criando_servidor_http_básico/index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const port = 5000
+
+const server = http.createServer((req, res)=>{
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write('<meta charset="UTF-8">');
+    res.write('<h1>Olá Mundo!</h1>');
+    res.end()
+});
+
+server.listen(port, () =>{
+    console.log(`Servidor rodando ${port}`);
+});
